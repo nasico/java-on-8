bed7723d0879183f2708f1807e033b249e6982b9
liqianlong <liqianlong@cnwansun.com>
2020-04-28 16:36:57 +0800

    init

---
+++ src/main/java/com/nasico/demo/unit1/Ligth.java
@@ -0,0 +1,19 @@
+package com.nasico.demo.unit1;
+
+public class Ligth {
+	public void on() {
+
+	}
+
+	public void off() {
+
+	}
+
+	public void birghten() {
+
+	}
+
+	public void dim() {
+
+	}
+}
---
+++ src/test/java/com/nasico/demo/JavaOn8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasico.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOn8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
